b10f2d4aee056c21c43ce4344047d91dbd8d242e
Sagnik Chakraborty <sagnikchk118@gmail.com>
2024-08-18 15:57:54 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import AuthInputs from './components/AuthInputs';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+}
+
+export default App;
