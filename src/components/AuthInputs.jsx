7427c99019f6ce19653d4f8649021c5332613693
Sagnik Chakraborty <sagnikchk118@gmail.com>
2024-08-20 01:09:31 +0530

    Update 1

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -20,33 +20,63 @@ function AuthInputs() {
   const emailNotValid = submitted && !enteredEmail.includes('@');
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
+  const labelStyle = `block mb-2 text-sm font-bold tracking-widest uppercase`;
+  const inputStyle =
+    'w-full py-3 px-4 leading-6 border-solid border-[1px] border-transparent rounded-[0.25rem] shadow-sm';
+
   return (
-    <div id="auth-inputs">
-      <div className="controls">
+    <div className="w-full max-w-[28rem] p-8 mx-auto my-0 mb-8 rounded-lg text-white shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col gap-10 mb-6">
         <p>
-          <label>Email</label>
+          <label
+            className={`${labelStyle} ${
+              emailNotValid ? 'text-red-400' : 'text-[#6b7280]'
+            }`}
+          >
+            Email
+          </label>
           <input
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            className={`${inputStyle} ${
+              emailNotValid
+                ? 'bg-red-100 text-red-500 border-red-300'
+                : 'bg-[#d1d5db] text-[#374151]'
+            }`}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
+          <label
+            className={`${labelStyle} ${
+              passwordNotValid ? 'text-red-400' : 'text-[#6b7280]'
+            }`}
+          >
+            Password
+          </label>
           <input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            className={`${inputStyle} ${
+              passwordNotValid
+                ? 'bg-red-100 text-red-500 border-red-300'
+                : 'bg-[#d1d5db] text-[#374151]'
+            }`}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
         </p>
       </div>
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className="flex justify-end gap-4">
+        <button
+          type="button"
+          className="text-amber-400 hover:text-amber-500 border-none font-mono"
+        >
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
+        <button
+          className="py-3 px-5 font-mono font-semibold uppercase rounded-lg text-stone-900 border-none bg-amber-400 hover:bg-amber-500"
+          onClick={handleLogin}
+        >
           Sign In
         </button>
       </div>
